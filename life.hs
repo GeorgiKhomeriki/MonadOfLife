fe5e6d5441b7d4b35e063b8a7e08b61edaf3d6ba
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-06-01 21:31:45 +0200

    added comments

--- life.hs
+++ life.hs
@@ -16,6 +16,8 @@ instance Show Cell where
     show Alive = "O"
     show Dead  = " "
 
+-- given width and height as arguments initializes a random World
+-- and starts the main loop
 main :: IO ()
 main = do 
     args <- getArgs
@@ -26,6 +28,7 @@ main = do
         cls
         loop $ initWorld (read $ head args) (read $ args !! 1)
 
+-- displays the World, evolves it and checks for input
 loop :: IO World -> IO ()
 loop world = do
     w @ (World sw sh cs) <- world
@@ -41,55 +44,66 @@ loop world = do
         Nothing -> step w
     where step w = cls >> loop (return $ evolve w)
 
+-- clears the screen and resets cursor position
 cls :: IO ()
 cls = clearScreen >> setCursorPosition 0 0
 
+-- given width and height, returns a random World
 initWorld :: Int -> Int -> IO World
 initWorld w h = do
     cells <- replicateM (w * h) initCell
     return $ World w h cells
 
+-- returns a random Cell
 initCell :: IO Cell
 initCell = do
     r <- randomRIO(0, 1) :: IO Int
     return $ if r == 0 then Alive else Dead
 
+-- given a World, return the World's next (evolved) state
 evolve :: World -> World
 evolve world @ (World w h cs) = World w h [evolveCell x y world | y <- ys, x <- xs]
     where
     xs = [0..w-1]
     ys = [0..h-1]
 
+-- given the coordinates of a cell in the given world returns that Cell's next state
 evolveCell :: Int -> Int -> World -> Cell
 evolveCell x y w
     | n == 2 && isAlive x y w || n == 3 = Alive
     | otherwise                         = Dead
     where n = countNeighbours x y w
 
+-- returns the number of adjacent living Cells to the Cell at (x, y)
 countNeighbours :: Int -> Int -> World -> Int
 countNeighbours x y w = length . filter (True==) . map (\(i, j) -> isAlive i j w) $ ls
     where ls = [(i, j) | i <- [x-1..x+1], j <- [y-1..y+1], i /= x || j /= y ]
 
+-- checks whether the Cell at (x, y) is dead or alive
 isAlive :: Int -> Int -> World -> Bool
 isAlive x y w = case c of
     Alive -> True
     Dead  -> False
     where c = getCell x y w
 
+-- returns the Cell that is on (x, y) in the given World
 getCell :: Int -> Int -> World -> Cell
 getCell x y (World w h cs)
     | isInside  = cs !! (y * w + x)
     | otherwise = Dead
     where isInside = x >= 0 && y >= 0 && x < w && y < h
 
+-- display the given World
 showWorld :: World -> IO ()
 showWorld w = putStrLn $!! readWorld w
 
+-- takes a World and returns it's String representation 
 readWorld :: World -> String
 readWorld (World _ _ []) = []
 readWorld (World w h cs) = concatMap show (take w cs) 
     ++ "\n" ++ readWorld (World w h $ drop w cs)
 
+-- given the list of current Cells, display's some information
 showInfo :: [Cell] -> IO ()
 showInfo cs = putStrLn ("(q - quit)     (r - reset)     live cells: " 
     ++ (show . length $ filter (Alive==) cs))
